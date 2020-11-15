15f585ea72a8c7c63068868c1b276e56ed8daca0
Tony Kim <sunghook0@gmail.com>
2020-11-15 15:21:28 +0900

    remove intermediates files

--- app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/mame4droid-jni/mame4droid-jni.o.d
+++
@@ -1,5 +0,0 @@
-C:/Users/golia/AndroidStudioProjects/MAME4droid/app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/mame4droid-jni/mame4droid-jni.o: \
-  C:/Users/golia/AndroidStudioProjects/MAME4droid/app/src/main/jni/mame4droid-jni.c \
-  C:/Users/golia/AndroidStudioProjects/MAME4droid/app/src/main/jni/com_seleuco_mame4droid_Emulator.h
-
-C:/Users/golia/AndroidStudioProjects/MAME4droid/app/src/main/jni/com_seleuco_mame4droid_Emulator.h:
--- cninja/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/mame4droid-jni/mame4droid-jni.o.d
+++
@@ -1,5 +0,0 @@
-/home/sunghook/work/DGCON/mame_cninja/android_part/cninja/cninja/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/mame4droid-jni/mame4droid-jni.o: \
-  /home/sunghook/work/DGCON/mame_cninja/android_part/cninja/cninja/src/main/jni/mame4droid-jni.c \
-  /home/sunghook/work/DGCON/mame_cninja/android_part/cninja/cninja/src/main/jni/com_seleuco_mame4droid_Emulator.h
-
-/home/sunghook/work/DGCON/mame_cninja/android_part/cninja/cninja/src/main/jni/com_seleuco_mame4droid_Emulator.h:
