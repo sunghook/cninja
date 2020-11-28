0f8bdffb2a7ada393e54391d6bb81c2caa0e9375
Tony Kim <sunghook0@gmail.com>
2020-11-28 20:57:09 +0900

    Revert "remove intermediates files"
    This reverts commit 15f585ea72a8c7c63068868c1b276e56ed8daca0.

---
+++ app/build/intermediates/ndkBuild/debug/obj/local/x86_64/objs-debug/mame4droid-jni/mame4droid-jni.o.d
@@ -0,0 +1,5 @@
+C:/Users/golia/AndroidStudioProjects/MAME4droid/app/build/intermediates/ndkBuild/debug/obj/local/x86_64/objs-debug/mame4droid-jni/mame4droid-jni.o: \
+  C:/Users/golia/AndroidStudioProjects/MAME4droid/app/src/main/jni/mame4droid-jni.c \
+  C:/Users/golia/AndroidStudioProjects/MAME4droid/app/src/main/jni/com_seleuco_mame4droid_Emulator.h
+
+C:/Users/golia/AndroidStudioProjects/MAME4droid/app/src/main/jni/com_seleuco_mame4droid_Emulator.h:
