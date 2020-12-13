095fdc1bad8cba8e86edb22a625d2944fb67340d
Tony Kim <sunghook0@gmail.com>
2020-12-14 00:49:45 +0900

    [CJINJA-003] AdMob - control full ad integration - add sleep between Emu.resume and EMu.insertCoin

--- cninja/src/main/java/com/seleuco/mame4droid/MAME4droid.java
+++ cninja/src/main/java/com/seleuco/mame4droid/MAME4droid.java
@@ -295,8 +295,16 @@ public class MAME4droid extends Activity {
 					load_fullAD();
 
 					Emulator.resume();
+
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+
 					int newtouches = COIN_VALUE;
 					Emulator.setPadData(0, newtouches);
+
 				}
 
 			});
